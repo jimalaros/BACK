97b6976f9d826aa85b5884ba0a04a725b40288ea
jimalaros <jimalaros25@gmail.com>
2022-03-06 19:13:57 -0500

    paypal

--- routes/Payment/index.js
+++ routes/Payment/index.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import mercadopago from "./mercadopago.js";
+import paypal from "./paypal.js";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const router = Router();
 
-//const paypal = require ('./paypal.js')
-
 router.use("/mercadopago", mercadopago);
-//router.use('/paypal', paypal);
+router.use("/paypal", paypal);
 
 export default router;
