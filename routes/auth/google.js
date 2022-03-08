e4fa0a9ab25b71a3fb5ca31cf905903509835d3f
jimalaros <jimalaros25@gmail.com>
2022-03-07 21:28:08 -0500

    auth

--- routes/auth/google.js
+++ routes/auth/google.js
@@ -21,13 +21,12 @@ router.get(
   function (req, res) {
     console.log(`Peticion get /${strategy_name}/callback `);
     const data = req.user._json;
-    console.log("Data:");
-    console.log(data);
-    const token = "hgjsd8fs6g7s7df67g6sdf43sdg2s3df5sg6s7df7";
+    res.json(data);
+    /**const token = "hgjsd8fs6g7s7df67g6sdf43sdg2s3df5sg6s7df7";
 
     const url_front = process.env.URL_FRONT + `/?token=${token}`;
 
-    res.redirect(301, url_front);
+    res.redirect(301, url_front);**/
   }
 );
 
